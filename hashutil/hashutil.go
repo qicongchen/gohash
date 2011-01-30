79d0192245cf5343090c1328c5d7f0544ca4eea5
John Asmuth <jasmuth@gmail.com>
2011-01-30 15:11:27 -0500

    added type for []float64 keys

--- hashutil/hashutil.go
+++ hashutil/hashutil.go
@@ -15,3 +15,25 @@ func HashFloat64Slice(fs []float64) (hash uint64) {
 	}
 	return
 }
+
+type Float64SliceHasher []float64
+func (this Float64SliceHasher) Hashcode() (hash uint64) {
+	return HashFloat64Slice(this)
+}
+func (this Float64SliceHasher) Compare(other Float64SliceHasher) int {
+	for i, v := range this {
+		if v < other[i] {
+			return -1
+		}
+		if v > other[i] {
+			return 1
+		}
+	}
+	return 0
+}
+func (this Float64SliceHasher) LessThan(oi interface{}) bool {
+	return this.Compare(oi.(Float64SliceHasher)) < 0
+}
+func (this Float64SliceHasher) Equals(oi interface{}) bool {
+	return this.Compare(oi.(Float64SliceHasher)) == 0
+}
